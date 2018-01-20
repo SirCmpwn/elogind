9dbba8bfbabe466fd2dfdb5056f9c688c39d0ca2
Drew DeVault <sir@cmpwn.com>
2018-01-20 17:28:38 -0500

    Define strndupa in string-util.h
    This is not available on musl libc.

--- src/basic/string-util.h
+++ src/basic/string-util.h
@@ -24,6 +24,15 @@
 #include <stddef.h>
 #include <string.h>
 
+#ifndef strndupa
+#define strndupa(s, n) \
+       (__extension__ ({const char *__in = (s);                    \
+                        size_t __len = strnlen (__in, (n)) + 1;    \
+                        char *__out = (char *) alloca (__len);     \
+                        __out[__len-1] = '\0';                     \
+                        (char *) memcpy (__out, __in, __len-1);}))
+#endif
+
 #include "macro.h"
 
 /* What is interpreted as whitespace? */
